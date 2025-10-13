c43287cbc5f1eff3efdc3900bbe308b1a15a91a2
Togo-bit <tadisaipratyush@gmail.com>
2025-10-13 15:41:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 # Connect to Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+import json, os
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Load credentials from environment variable instead of file
+google_creds_json = os.getenv("GOOGLE_CREDENTIALS")
+creds_dict = json.loads(google_creds_json)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
 client = gspread.authorize(creds)
 sheet = client.open("form_responses").sheet1  # replace with your sheet name
 
@@ -25,3 +32,4 @@ def form():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # use Render’s port or default 5000
     app.run(host='0.0.0.0', port=port)
+
